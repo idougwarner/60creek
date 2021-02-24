448aca06379e61aacd51be0a27ad39e26b33fc70
idougwarner <idougwarner@gmail.com>
2021-02-23 20:37:14 -0500

    remove redundant files

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import Menu from '../../components/Menu'
-import './About.scss'
+import React from "react";
+import "./About.scss";
 
 const About = () => {
   return (
     <div className="about">
-      <Menu />
-      <div className='g-page-background-with-nav'>
-        <h1>About Us</h1>
-        <p>Learn more about us</p>
-      </div>
+      <h1>About Us</h1>
+      <p>Learn more about us</p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
